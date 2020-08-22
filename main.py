803ffdf3ff461c80fcc2b7dded6d65fbe502fe38
Felipe Romero <jufromerosa@unal.edu.co>
2020-08-21 21:06:31 -0500

    Multi_Tracking Approach

--- main.py
+++ main.py
@@ -105,6 +105,7 @@ while True:
 	if key == ord("s"):
 		# select the bounding box of the object we want to track (make
 		# sure you press ENTER or SPACE after selecting the ROI)
+		
 		initBB = cv2.selectROI("Frame", frame, fromCenter=False,
 			showCrosshair=True)
 		# start OpenCV object tracker using the supplied bounding box
